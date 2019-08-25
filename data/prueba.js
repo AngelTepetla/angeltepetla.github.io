418417b3ba52642b649facdb083b76d03f84f708
Ángel Tepetla <megadiego1@gmail.com>
2019-08-25 00:01:59 -0500

    colores

--- data/prueba.js
+++ data/prueba.js
@@ -1,91 +1,58 @@
 
 
 var texto = document.getElementById("nombre");
+var texto_nombre = document.getElementById("mi_nombre");
+var li_color = document.getElementById("li_activa");
+
+
 var texto_yo = document.getElementById("tipo_yo");
 
-        //my_cliente.innerHTML = "¡Hola " + ipAddress + ", " + "me alegra tenerte aquí!";
+var incColor = 0;
 
+function color(hsv){
+
+
+    var auxColor = 1;
+
+    texto.style.backgroundColor = "hsl(" + incColor + ", 100%, 50%";
+    texto.style.borderColor = "hsl(" + incColor + ", 100%, 50%";
+
+    texto_nombre.style.backgroundColor = "hsl(" + incColor + ", 100%, 50%";
+    texto_nombre.style.borderColor = "hsl(" + incColor + ", 100%, 50%";
+
+    texto_yo.style.backgroundColor = "hsl(" + incColor + ", 100%, 50%";
+    texto_yo.style.borderColor = "hsl(" + incColor + ", 100%, 50%";
+
+    li_color.style.backgroundColor = "hsl(" + incColor + ", 85%, 50%";
+
+    //console.log(incColor);
+    //incColor = incColor + 1;
+    incColor = incColor + hsv;
+
+    if (incColor >= 360) { incColor = 0;}
 /*
-var tipo = ["angel", "david", "meloso", "abogado"];
-var caracteres = ["a", "n", "g", "e", ""];
-var conta = 0;
+    var cuentaColor = setInterval(function incrementoColor() {
+
+        incColor = incColor + 1;
+
+        if(auxColor >= 360){
+            clearInterval(cuentaColor);
+        }
+
+        texto.style.backgroundColor = "hsl(" + incColor + ", 100%, 50%";
+        console.log(incColor);
+
+        //texto.style.backgroundColor="hsl(" + 200 + ", 100%,50%)";
+
+        auxColor++;
+    }, 1000);
 */
-
-
-/*
-function cambia(){
-
-    var inc = 0;
-
-    var cuenta = setInterval(function incremento() {
-        //console.log(inc + "inc");
-        inc = inc + 0.2;
-
-        if(inc >= 1){
-            clearInterval(cuenta);
-        }
-
-
-        texto.style.opacity = inc;
-        texto.innerHTML = tipo[conta];
-        //letra = letra + tipo[aux];
-    }, 100);
-
-
-
-    //texto.style.opacity = 0.1;
-    //texto.innerHTML = tipo[conta];
-    console.log('conta' + conta);
-    conta++;
-
-    if(conta >= 4){
-        conta = 0;
-    }
-
-
-
-}*/
-
-
-/*
-function palabra(){
-
-    var inc = 0;
-    conta = 0;
-    var letra = caracteres[conta];
-    var aux = 1;
-
-
-    var cuenta = setInterval(function incremento() {
-        //console.log(inc + "inc");
-        inc = inc + 0.2;
-
-        if(inc >= 1){
-            clearInterval(cuenta);
-        }
-
-
-        //texto.style.opacity = inc;
-        texto.innerHTML = letra;
-        letra = letra + caracteres[aux];
-        aux++;
-    }, 500);
-
-
-
-    //texto.style.opacity = 0.1;
-    //texto.innerHTML = tipo[conta];
-    console.log('conta' + conta);
-    conta++;
-
-    if(conta >= 4){
-        conta = 0;
-    }
-
-
-
 }
-*/
+
+
+//setInterval(color, 50);
+
+
 
 function palabra(word){
 
@@ -99,6 +66,7 @@ function palabra(word){
 
     ////console.log(tamy);
 
+    //color(30);
     var cuenta = setInterval(function incremento() {
         //console.log(inc + "inc");
         inc = inc + 0.2;
@@ -133,7 +101,7 @@ function palabra2(wordy){
     var letra22 = caracteres22[conta22];
     var aux22 = 1;
 
-
+    color(30);
     var cuenta22 = setInterval(function incremento2() {
         inc22 = inc22 + 0.2;
 
@@ -168,17 +136,13 @@ function textos33(){
     palabra2( tipos[contaAux] );
 }
 
-/*
-for(var i = 0; i<10; i++){
-    console.log('hola' + i);
-    //texto.innerHTML = "Me llamo" + i;
-    //set.setTimeout(function() {}, 10);
-}
-*/
+
 
 setInterval(textos, 3000);
 setInterval(textos33, 4000);
 //setInterval(off, )
+
+
 
 
 var tras = 0;
@@ -204,31 +168,6 @@ setTimeout(function firma(){
 
 
 }, 500);
-
-/*
-
-setTimeout(function firma(){
-    var cuadro = document.getElementById("firma_yo");
-    //cuadro.style.visibility = "hidden";
-
-    cuadro.style.transform = "translateY(900px)";
-    var cuenta2 = setInterval(function incremento() {
-        //console.log(inc + "inc");
-        opa = opa - 0.05;
-
-        if(opa <= 0){
-            clearInterval(cuenta2);
-        }
-
-        cuadro.style.opacity = opa;
-        //texto.innerHTML = tipo[conta];
-        //letra = letra + tipo[aux];
-    }, 25);
-
-
-}, 2000);
-*/
-
 
 
 
@@ -309,75 +248,3 @@ function textos2(){
 setInterval(textos2, 4000);
 //setInterval(off, )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var parrafo = document.getElementById("tepetla");
-parrafo.onmouseover=
-*/
-
-
-
-/*
-document.getElementById("tepetla").onmouseover = function(){ abajo() };
-document.getElementById("tepetla").onmouseout = function(){ arriba() };
-
-var tras2 = 0;
-var bandera = 0;
-function abajo(){
-    var parrafo = document.getElementById("tepetla");
-    var cuenta3 = setInterval(function incremento3() {
-        //console.log(inc + "inc");
-        tras2 = tras2 + 70;
-
-        if(tras2 >= 400){
-            clearInterval(cuenta3);
-            parrafo.style.visibility = "visible";
-        }
-
-        ///cuadro.style.opacity = opa;
-        //texto.innerHTML = tipo[conta];
-        //letra = letra + tipo[aux];
-        parrafo.style.transform = "translateY(" + tras2 + "px)";
-    }, 25);
-    bandera = 1;
-}
-
-//var tras2 = 0;
-function arriba(){
-    if(bandera == 1) {
-        var parrafo = document.getElementById("tepetla");
-        var cuenta3 = setInterval(function incremento3() {
-            //console.log(inc + "inc");
-            tras2 = tras2 - 70;
-
-            if(tras2 <= 0){
-                clearInterval(cuenta3);
-                parrafo.style.visibility = "visible";
-            }
-
-            ///cuadro.style.opacity = opa;
-            //texto.innerHTML = tipo[conta];
-            //letra = letra + tipo[aux];
-            parrafo.style.transform = "translateY(" + tras2 + "px)";
-        }, 25);
-    }
-
-}
-*/

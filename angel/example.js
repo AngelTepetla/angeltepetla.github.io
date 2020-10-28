c2474798549c8e65ee22e9fb53df426ef735cea8
J. Ángel Tepetla <megadiego1@gmail.com>
2020-10-28 16:28:08 -0600

    Cambios 20
    Cambio diseño 28/10, minimacion

--- angel/example.js
+++
@@ -1,9 +0,0 @@
-new fullpage('#fullpage', {
-    sectionsColor: ['#1d88dc', '#444', '#ff2266', '#fff', '#141414'],
-});
-
-
-//#C0C0C0
-//new fullpage('#fullpage', {
-// sectionsColor: ['#1d88dc', 'white', '#C0C0C0', 'orange'],
-//});
--- data/colors.js
+++
@@ -1,9 +0,0 @@
-new fullpage('#fullpage', {
-  sectionsColor: ['#1d88dc'],
-});
-
-
-//#C0C0C0
-//new fullpage('#fullpage', {
- // sectionsColor: ['#1d88dc', 'white', '#C0C0C0', 'orange'],
-//});
